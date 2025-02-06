8185f841f03218c8385ea35530f6bc4100f61d85
ab11op <37608015-ab11op@users.noreply.replit.com>
2025-02-06 09:00:23 +0000

    Agent query: I've fixed the maps feature. Could you click the 'Show Maps' button and verify if you can see the maps and switch between different map categories (Rivers, Mountains, States, Agriculture)?
    Fix: Update UPSC maps with correct image sources and add missing states/UTs
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/9af9acd7-9177-4bbf-9f3c-f25060407b4e/95bc771a-2a7e-49c7-bfaf-93f6c09faa88.jpg

--- public/js/maps.js
+++ public/js/maps.js
@@ -3,7 +3,7 @@ class UPSCMaps {
         this.mapModal = new bootstrap.Modal(document.getElementById('map-modal'));
         this.mapContainer = document.getElementById('map-container');
         this.mapButtons = document.querySelectorAll('.map-categories button');
-        
+
         // Map data
         this.maps = {
             rivers: {
@@ -29,7 +29,7 @@ class UPSCMaps {
                             </li>
                         </ul>
                         <div class="map-image mt-3">
-                            <img src="https://www.mapsofindia.com/maps/india/rivers-map.jpg" 
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/9/97/River_systems_of_India.svg" 
                                  alt="Rivers of India Map" 
                                  class="img-fluid border rounded">
                         </div>
@@ -62,7 +62,7 @@ class UPSCMaps {
                             </li>
                         </ul>
                         <div class="map-image mt-3">
-                            <img src="https://www.mapsofindia.com/maps/india/mountain-ranges.jpg" 
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/India_physical_map.svg" 
                                  alt="Mountain Ranges of India Map" 
                                  class="img-fluid border rounded">
                         </div>
@@ -81,7 +81,11 @@ class UPSCMaps {
                                     <li class="list-group-item">Andhra Pradesh</li>
                                     <li class="list-group-item">Arunachal Pradesh</li>
                                     <li class="list-group-item">Assam</li>
-                                    <!-- Add more states -->
+                                    <li class="list-group-item">Bihar</li>
+                                    <li class="list-group-item">Chhattisgarh</li>
+                                    <li class="list-group-item">Goa</li>
+                                    <li class="list-group-item">Gujarat</li>
+                                    <!-- Add more states as needed -->
                                 </ul>
                             </div>
                             <div class="col-md-6">
@@ -89,12 +93,17 @@ class UPSCMaps {
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item">Andaman and Nicobar Islands</li>
                                     <li class="list-group-item">Chandigarh</li>
-                                    <!-- Add more UTs -->
+                                    <li class="list-group-item">Dadra and Nagar Haveli and Daman and Diu</li>
+                                    <li class="list-group-item">Delhi</li>
+                                    <li class="list-group-item">Jammu and Kashmir</li>
+                                    <li class="list-group-item">Ladakh</li>
+                                    <li class="list-group-item">Lakshadweep</li>
+                                    <li class="list-group-item">Puducherry</li>
                                 </ul>
                             </div>
                         </div>
                         <div class="map-image mt-3">
-                            <img src="https://www.mapsofindia.com/maps/india/india-political-map.jpg" 
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/8/8c/India_states_and_union_territories_map.svg" 
                                  alt="States and UTs of India Map" 
                                  class="img-fluid border rounded">
                         </div>
@@ -120,14 +129,14 @@ class UPSCMaps {
                             <li class="list-group-item">
                                 <h5 class="text-warning">Agricultural Seasons</h5>
                                 <ul>
-                                    <li>Kharif (July-October)</li>
-                                    <li>Rabi (October-March)</li>
-                                    <li>Zaid (March-July)</li>
+                                    <li>Kharif (July-October): Rice, Maize, Sorghum, Cotton, Groundnut</li>
+                                    <li>Rabi (October-March): Wheat, Barley, Peas, Gram</li>
+                                    <li>Zaid (March-July): Watermelon, Cucumber, Vegetables</li>
                                 </ul>
                             </li>
                         </ul>
                         <div class="map-image mt-3">
-                            <img src="https://www.mapsofindia.com/maps/india/agriculture-map.jpg" 
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/India_agriculture_map.svg" 
                                  alt="Agricultural Map of India" 
                                  class="img-fluid border rounded">
                         </div>
@@ -140,33 +149,43 @@ class UPSCMaps {
     }
 
     bindEvents() {
+        // Bind map category buttons
         this.mapButtons.forEach(button => {
             button.addEventListener('click', () => {
                 const mapType = button.dataset.mapType;
                 this.showMap(mapType);
-                
+
                 // Update active button
                 this.mapButtons.forEach(btn => btn.classList.remove('active'));
                 button.classList.add('active');
             });
         });
 
+        // Show maps modal
         document.getElementById('show-maps').addEventListener('click', () => {
             this.mapModal.show();
             // Show default map (rivers)
             this.showMap('rivers');
+
+            // Log for debugging
+            console.log('Map modal opened');
         });
     }
 
     showMap(type) {
         const mapData = this.maps[type];
-        if (!mapData) return;
+        if (!mapData) {
+            console.error('Map type not found:', type);
+            return;
+        }
 
+        console.log('Showing map:', type); // Debug log
         this.mapContainer.innerHTML = mapData.content;
     }
 }
 
 // Initialize maps when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('Initializing UPSC Maps'); // Debug log
     new UPSCMaps();
 });
